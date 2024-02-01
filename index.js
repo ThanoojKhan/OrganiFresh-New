2a9dd09e568fadb24522848107d98d2cc199fa30
Thanooj <thanu1495@gmail.com>
2024-02-01 06:45:12 +0530

    style format

--- index.js
+++ index.js
@@ -1,4 +1,3 @@
-const mongoose = require('mongoose')
 const morgan = require('morgan')
 const userRoute = require('./router/userRoute')
 const adminRoute = require('./router/adminRoute')
@@ -6,17 +5,15 @@ const bodyParser = require('body-parser')
 const session = require('express-session')
 const nocache = require('nocache')
 const path = require('path')
-require('dotenv').config();
 
+require('dotenv').config();
 const config = require('./config/config')
 config.mongooseConnection()
-
 
 const express = require('express')
 const app = express()
 app.use(morgan("dev"));
 app.use(nocache())
-
 
 app.use(session({ secret:process.env.SECRET_KEY, cookie: { maxAge: 60000 * 100 }, saveUninitialized: true, resave: true }))
 app.use(express.static(path.join(__dirname, 'public')));
